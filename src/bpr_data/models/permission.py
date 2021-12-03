32e9a0e8a59da2f3b86f630fddff5d047e029701
rpetro79 <ralucapetrovici@yahoo.com>
2021-12-03 14:56:51 +0100

    fix

--- src/bpr_data/models/permission.py
+++ src/bpr_data/models/permission.py
@@ -1,11 +1,13 @@
 from enum import Enum, auto
 
+from bpr_data.models.mongo_document_base import SerializableObject
 
-class WorkspacePermission(Enum):
+
+class WorkspacePermission(Enum, SerializableObject):
     MANAGE_TEAMS = "MANAGE_TEAMS"
     MANAGE_PERMISSIONS = "MANAGE_PERMISSIONS"
     MANAGE_WORKSPACE = "MANAGE_WORKSPACE"
     MANAGE_USERS = "MANAGE_USERS"
 
-class ProjectPermission(Enum):
+class ProjectPermission(Enum, SerializableObject):
     MANAGE_PROJECTS = "MANAGE_PROJECTS"
