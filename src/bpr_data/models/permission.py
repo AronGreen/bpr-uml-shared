7b2c372dc918c8ccd69cf2c23e5d6de81450eb6f
rpetro79 <ralucapetrovici@yahoo.com>
2021-12-13 10:19:01 +0100

    added conversion

--- src/bpr_data/models/permission.py
+++ src/bpr_data/models/permission.py
@@ -7,3 +7,9 @@ class WorkspacePermission(str, Enum):
     MANAGE_TEAMS = "MANAGE_TEAMS"
     MANAGE_PERMISSIONS = "MANAGE_PERMISSIONS"
     MANAGE_WORKSPACE = "MANAGE_WORKSPACE"
+
+    def convert_strings_to_workspace_permissions_enums(permissions: list):
+        enum_permissions = []
+        for permission in permissions:
+            enum_permissions.append(WorkspacePermission(permission))
+        return enum_permissions
